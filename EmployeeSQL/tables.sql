1640040d48b2db87f5dbcda5106f1e3ea59bd8fb
microdecision <hakamybeth@gmail.com>
2020-11-15 11:48:38 -0600

    added the employee folder

---
+++ EmployeeSQL/tables.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE "Employees" (
+"emp_no" int   NOT NULL,
+"emp_title_id" varchar   NOT NULL,
+"birth_date" date   NOT NULL,
+"first_name" varchar   NOT NULL,
+"last_name" varchar   NOT NULL,
+"sex" varchar   NOT NULL,
+"hire_date" date   NOT NULL, 
+);
